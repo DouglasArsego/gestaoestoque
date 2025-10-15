6e3a3c7b0115de048e999431e690e7679e31f92a
DouglasArsego <douglasarsego14@gmail.com>
2025-10-15 09:56:33 -0300

    Rotas e Menu Pagina
    Menu da pagina principal

--- estoque-app/src/app/app.routes.ts
+++ estoque-app/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { ProdutoList } from './produtos/produto-list';
 import { ProdutoForm } from './produtos/produto-form';
 import { CategoriaList } from './categorias/categoria-list';
 import { CategoriaForm } from './categorias/categoria-form';
+import { MovimentacaoList } from './movimentacao/movimentacao-list';
+import { MovimentacaoForm } from './movimentacao/movimentacao-form';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'produtos', pathMatch: 'full' },
@@ -11,5 +14,8 @@ export const routes: Routes = [
   { path: 'produtos/editar/:id', component : ProdutoForm },
   { path: 'categorias', component : CategoriaList },
   { path: 'categorias/novo', component : CategoriaForm },
-  { path: 'categorias/editar/:id', component : CategoriaForm }
+  { path: 'categorias/editar/:id', component : CategoriaForm },
+  { path: 'movimentacoes', component: MovimentacaoList },
+  { path: 'movimentacoes/nova', component: MovimentacaoForm }
+
 ];
