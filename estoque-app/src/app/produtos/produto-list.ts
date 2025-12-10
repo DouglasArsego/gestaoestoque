8d9ebc2888e49d5b1d301123af59a6bd5169f836
DouglasArsego <douglasarsego14@gmail.com>
2025-12-10 20:03:20 -0300

    Imagem Supabase
    Carregar imagem para o storage supabase.

--- estoque-app/src/app/produtos/produto-list.ts
+++ estoque-app/src/app/produtos/produto-list.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Produto, ProdutoService } from './produto';
 import { FiltroPipe } from '../shared/filtro-pipe';
 import { Categoria, CategoriaService } from '../categorias/categoria';
+import { supabase } from '../supabase.client';
 
 @Component({
   selector: 'app-produto-list',
@@ -19,6 +20,9 @@ export class ProdutoList {
   produtos: Produto[] = [];
   categorias: Categoria[] = [];
   filtro = '';
+
+  // referencia ao input file escondido
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
   constructor() {
     this.carregarCategorias();
@@ -48,5 +52,55 @@ export class ProdutoList {
         this.carregarProdutos();
       });
     }
+  }
+
+
+  abrirInputArquivo(produtoId: string) {
+    const input = this.fileInput.nativeElement;
+
+  
+    input.dataset['produtoId'] = produtoId;
+
+    input.value = ''; 
+    input.click();    
+  }
+
+  async uploadImagem(event: Event, produtoIdFromRow?: string) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+
+    const produtoId = produtoIdFromRow ?? input.dataset['produtoId'];
+
+    if (!produtoId) {
+      alert("Erro: ID do produto não encontrado.");
+      return;
+    }
+
+   
+    const filePath = `produtos/${produtoId}/${Date.now()}_${file.name.replace(/\s+/g, '_')}`;
+
+    // faz o upload
+    const { data, error } = await supabase.storage
+      .from('fotos')  
+      .upload(filePath, file, {
+        cacheControl: '3600',
+        upsert: false
+      });
+
+    if (error) {
+      console.error(error);
+      alert("Erro ao enviar imagem: " + error.message);
+    } else {
+      console.log("Imagem enviada:", data);
+      alert("Imagem enviada com sucesso!");
+    }
+
+    input.value = ''; 
   }
 }
