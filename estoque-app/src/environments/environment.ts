f4274216faf9cd5ede5e6bebc6bab03d39f0e04d
DouglasArsego <douglasarsego14@gmail.com>
2025-12-11 17:18:45 -0300

    teste com chaves

---
+++ estoque-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://ejkxansxpnvtklfyauyg.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqa3hhbnN4cG52dGtsZnlhdXlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NTc3NDQsImV4cCI6MjA4MDIzMzc0NH0.xZnqogWsnLyIOcfqZqVH-wxDp7mDwWxCMIQU1wNJc8M'
+}
